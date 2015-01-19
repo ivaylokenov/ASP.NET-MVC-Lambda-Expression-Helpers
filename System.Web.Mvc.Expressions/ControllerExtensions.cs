d36adbee0b695350e245b51432b3f90ce3b3608b
ivaylokenov <ivaylo.kenov@gmail.com>
2015-01-19 11:07:43 +0200

    Added option to redirect to different controller

--- System.Web.Mvc.Expressions/ControllerExtensions.cs
+++ System.Web.Mvc.Expressions/ControllerExtensions.cs
@@ -10,6 +10,19 @@
         public static RedirectToRouteResult RedirectToAction<TController>(this TController controller, Expression<Action<TController>> action, object routeValues = null)
             where TController : Controller
         {
+            return GetRedirectFromExpression(action, routeValues);
+        }
+
+        public static RedirectToRouteResult RedirectToAction<TController, TRedirectController>(this TController controller, Expression<Action<TRedirectController>> action, object routeValues = null)
+            where TController : Controller
+            where TRedirectController : Controller
+        {
+            return GetRedirectFromExpression(action, routeValues);
+        }
+
+        private static RedirectToRouteResult GetRedirectFromExpression<TRedirectController>(Expression<Action<TRedirectController>> action, object routeValues = null)
+            where TRedirectController : Controller
+        {
             var routeValuesDict = new RouteValueDictionary(routeValues);
             routeValuesDict.AddRoutesFromExpression(action);
 
