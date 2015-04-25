acc84a35835dad6541d420e7d669f12ac6aeee42
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2015-04-25 23:29:38 +0300

    Implemented Ajax.BeginForm extension.

---
+++ System.Web.Mvc.Expressions/AjaxHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Web.Mvc.Expressions
+{
+    using System.Linq.Expressions;
+    using System.Web.Mvc.Ajax;
+    using System.Web.Mvc.Expressions.Internals;
+    using System.Web.Mvc.Html;
+
+    public static class AjaxHelperExtensions
+    {
+        public static MvcForm BeginForm<TController>(
+            this AjaxHelper helper, 
+            Expression<Action<TController>> action, 
+            object routeValues = null,
+            AjaxOptions ajaxOptions = null, 
+            object htmlAttributes = null) where TController : Controller
+        {
+            var routeInfo = RouteInformation.FromExpression(action, routeValues);
+            return helper.BeginForm(routeInfo.ActionName, routeInfo.ControllerName, routeValues, ajaxOptions, htmlAttributes);
+        }
+    }
+}
