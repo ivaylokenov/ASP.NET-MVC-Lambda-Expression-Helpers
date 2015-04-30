ef3a28096a35fb2d601fa1e894e5445d938964cf
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-04-30 15:34:44 +0300

    Implemented RedirectToActionPermanent extension methods for Controller.

--- System.Web.Mvc.Expressions/ControllerExtensions.cs
+++ System.Web.Mvc.Expressions/ControllerExtensions.cs
@@ -25,14 +25,33 @@
             return GetRedirectFromExpression(action, routeValues);
         }
 
+        public static RedirectToRouteResult RedirectToActionPermanent<TController>(
+            this TController controller,
+            Expression<Action<TController>> action,
+            object routeValues = null) where TController : Controller
+        {
+            return GetRedirectFromExpression(action, routeValues, true);
+        }
+
+        public static RedirectToRouteResult RedirectToActionPermanent<TController, TRedirectController>(
+                this TController controller,
+                Expression<Action<TRedirectController>> action,
+                object routeValues = null)
+            where TController : Controller
+            where TRedirectController : Controller
+        {
+            return GetRedirectFromExpression(action, routeValues, true);
+        }
+
         private static RedirectToRouteResult GetRedirectFromExpression<TRedirectController>(
             Expression<Action<TRedirectController>> action,
-            object routeValues = null) where TRedirectController : Controller
+            object routeValues = null,
+            bool permanent = false) where TRedirectController : Controller
         {
             var routeValuesDict = new RouteValueDictionary(routeValues);
             routeValuesDict.AddRoutesFromExpression(action);
 
-            return new RedirectToRouteResult(routeValuesDict);
+            return new RedirectToRouteResult(null, routeValuesDict, permanent);
         }
     }
 }
