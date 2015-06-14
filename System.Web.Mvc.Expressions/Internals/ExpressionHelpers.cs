87b61040b8c676fe74badfad5d394918a60022d4
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-06-14 16:09:14 +0300

    Improved the AddModelError extension methods with expressions with only TModel generic.

---
+++ System.Web.Mvc.Expressions/Internals/ExpressionHelpers.cs
@@ -0,0 +1,82 @@
+﻿namespace System.Web.Mvc.Expressions.Internals
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    internal static class ExpressionHelpers
+    {
+        public static string GetExpressionText(LambdaExpression expression)
+        {
+            var result = string.Empty;
+            if (expression.Body.NodeType == ExpressionType.Convert)
+            {
+                var unaryExpression = expression.Body as UnaryExpression;
+                if (unaryExpression != null)
+                {
+                    result = string.Join(".", GetProperties(unaryExpression.Operand));
+                }
+            }
+            else
+            {
+                result = ExpressionHelper.GetExpressionText(expression);
+            }
+
+            return result;
+        }
+
+        public static bool IsSingleArgumentIndexer(MethodCallExpression methodExpression)
+        {
+            var result = methodExpression != null &&
+                methodExpression.Arguments.Count == 1 &&
+                methodExpression.Method.DeclaringType
+                    .GetDefaultMembers()
+                    .OfType<PropertyInfo>()
+                    .Any(p => p.GetGetMethod() == methodExpression.Method);
+
+            return result;
+        }
+
+        private static IEnumerable<string> GetProperties(Expression expression)
+        {
+            var memberExpression = expression as MemberExpression;
+            if (memberExpression == null)
+            {
+                var methodExpression = expression as MethodCallExpression;
+                if (methodExpression != null && IsSingleArgumentIndexer(methodExpression))
+                {
+                    var indexerMemberExpression = methodExpression.Object as MemberExpression;
+                    if (indexerMemberExpression != null)
+                    {
+                        var index = methodExpression.Arguments
+                            .Select(arg => Expression.Convert(arg, typeof(object)))
+                            .Select(exp => Expression.Lambda<Func<object>>(exp, null).Compile()())
+                            .First();
+
+                        var propertyName = string.Format(
+                                "{0}.{1}[{2}]",
+                                string.Join(".", GetProperties(indexerMemberExpression.Expression)),
+                                indexerMemberExpression.Member.Name,
+                                index);
+
+                        yield return propertyName.TrimStart('.');
+                    }
+                }
+
+                yield break;
+            }
+
+            foreach (var propertyInfo in GetProperties(memberExpression.Expression))
+            {
+                yield return propertyInfo;
+            }
+
+            var property = memberExpression.Member as PropertyInfo;
+            if (property != null)
+            {
+                yield return property.Name;
+            }
+        }
+    }
+}
--- System.Web.Mvc.Expressions/ModelStateDictionaryExtensions.cs
+++ System.Web.Mvc.Expressions/ModelStateDictionaryExtensions.cs
@@ -1,33 +1,70 @@
 ﻿namespace System.Web.Mvc.Expressions
 {
-    using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
+    using System.Web.Mvc.Expressions.Internals;
 
     public static class ModelStateDictionaryExtensions
     {
-        public static void AddModelError<TModel, TProperty>(
+        public static void AddModelError<TModel>(
             this ModelStateDictionary modelStateDictionary,
-            Expression<Func<TModel, TProperty>> keyExpression,
+            Expression<Func<TModel, object>> keyExpression,
             string errorMessage)
         {
             ValidateKeyExpression(keyExpression);
 
-            var key = ExpressionHelper.GetExpressionText(keyExpression);
+            var key = ExpressionHelpers.GetExpressionText(keyExpression);
             var modelState = GetModelStateForKey(modelStateDictionary, key);
             modelState.Errors.Add(errorMessage);
         }
 
-        public static void AddModelError<TModel, TProperty>(
+        public static void AddModelError<TModel>(
             this ModelStateDictionary modelStateDictionary,
-            Expression<Func<TModel, TProperty>> keyExpression,
+            Expression<Func<TModel, object>> keyExpression,
             Exception exception)
         {
             ValidateKeyExpression(keyExpression);
 
-            var key = ExpressionHelper.GetExpressionText(keyExpression);
+            var key = ExpressionHelpers.GetExpressionText(keyExpression);
             var modelState = GetModelStateForKey(modelStateDictionary, key);
             modelState.Errors.Add(exception);
+        }
+
+        private static void ValidateKeyExpression(LambdaExpression keyExpression)
+        {
+            if (keyExpression == null)
+            {
+                throw new ArgumentNullException("keyExpression");
+            }
+
+            var isValid = false;
+
+            if (keyExpression.Body.NodeType == ExpressionType.Convert)
+            {
+                var unaryExpression = keyExpression.Body as UnaryExpression;
+
+                isValid = unaryExpression != null &&
+                    (unaryExpression.Operand is MemberExpression ||
+                        (unaryExpression.Operand is MethodCallExpression &&
+                        ExpressionHelpers.IsSingleArgumentIndexer((MethodCallExpression)unaryExpression.Operand)));
+            }
+            else if (keyExpression.Body.NodeType == ExpressionType.Call)
+            {
+                var methodExpression = keyExpression.Body as MethodCallExpression;
+
+                isValid = ExpressionHelpers.IsSingleArgumentIndexer(methodExpression);
+            }
+            else if (keyExpression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                var memberExpression = keyExpression.Body as MemberExpression;
+
+                isValid = memberExpression != null && memberExpression.Member is PropertyInfo;
+            }
+
+            if (!isValid)
+            {
+                throw new ArgumentOutOfRangeException("keyExpression", "keyExpression should refer to a member property or indexer.");
+            }
         }
 
         private static ModelState GetModelStateForKey(ModelStateDictionary modelStateDictionary, string key)
@@ -40,44 +77,6 @@
             }
 
             return modelState;
-        }
-
-        private static void ValidateKeyExpression<TModel, TProperty>(Expression<Func<TModel, TProperty>> keyExpression)
-        {
-            if (keyExpression == null)
-            {
-                throw new ArgumentNullException("keyExpression");
-            }
-
-            var methodExpression = keyExpression.Body as MethodCallExpression;
-            if (methodExpression == null)
-            {
-                var memberExpression = keyExpression.Body as MemberExpression;
-                if (memberExpression == null)
-                {
-                    throw new ArgumentOutOfRangeException("keyExpression", "keyExpression should refer to a model member.");
-                }
-
-                if (!(memberExpression.Member is PropertyInfo))
-                {
-                    throw new ArgumentOutOfRangeException("keyExpression", "keyExpression should refer to a model property.");
-                }
-            }
-            else if (!IsSingleArgumentIndexer(methodExpression))
-            {
-                throw new ArgumentOutOfRangeException("keyExpression", "keyExpression should refer to a model member or indexer.");
-            }
-        }
-
-        private static bool IsSingleArgumentIndexer(MethodCallExpression methodExpression)
-        {
-            var result = methodExpression.Arguments.Count == 1 &&
-                methodExpression.Method.DeclaringType
-                    .GetDefaultMembers()
-                    .OfType<PropertyInfo>()
-                    .Any(p => p.GetGetMethod() == methodExpression.Method);
-
-            return result;
         }
     }
 }
